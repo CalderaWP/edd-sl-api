249efb1b62e743ea99af6d658fe8f5784e5a9f62
Josh Pollock <Josh@JoshPress.net>
2016-06-20 20:43:29 -0400

    improve errors

--- CWP_EDD_SL_API_Route.php
+++ CWP_EDD_SL_API_Route.php
@@ -99,6 +99,10 @@ class CWP_EDD_SL_API_Route {
 	 * @return mixed|WP_REST_Response
 	 */
 	public function get_licenses( WP_REST_Request $request ){
+		if( 0 == get_current_user_id() ) {
+			return $this->return_error( 403, 'You must be logged in' );
+		}
+		
 		if( 'full' ==  $request[ 'return' ] ){
 			$names_only = false;
 		}else{
@@ -180,16 +184,33 @@ class CWP_EDD_SL_API_Route {
 	}
 
 	/**
+	 * Return an error
+	 *
+	 * @since 0.1.0.
+	 *
+	 * @param int $code Optional. Status code. default is 500
+	 * @param mixed $data Optional. Data to return. Default is empty string.
+	 *
+	 * @return WP_REST_Response
+	 */
+	protected function return_error( $code = 500, $data = '' ){
+		$response = new WP_REST_Response( $data );
+		$response->set_status( $code );
+		return $response;
+
+	}
+
+	/**
 	 * Return a 404
 	 * 
 	 * @since 0.1.0
+	 *
+	 * @param string $message Optional. Error message. Default is empty string.
 	 * 
 	 * @return WP_REST_Response
 	 */
-	protected function return_404(){
-		$response = new WP_REST_Response;
-		$response->set_status( 404 );
-		return $response;
+	protected function return_404( $message = ''){
+		return $this->return_error( 404, $message );
 	}
 
 	/**
@@ -200,8 +221,6 @@ class CWP_EDD_SL_API_Route {
 	 * @return WP_REST_Response
 	 */
 	protected function return_not_yet(){
-		$response = new WP_REST_Response();
-		$response->set_status( 501 );
-		return $response;
+		return $this->return_error( 501 , __( 'Not implemented', 'cwp-edd-sl-api') );
 	}
 }
